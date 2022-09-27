296f05d6af2c93a206a64efbcd0f175e45548cdd
Jhonattan Meira dos Santos <jhonattantecinf@gmail.com>
2022-09-26 21:21:38 -0300

    Change Cookie Hash

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'HashYiiFrameworkPecaAgora',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
